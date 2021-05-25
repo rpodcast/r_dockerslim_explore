1b8505351a38550d41b4c72ddfb8eb70678c9214
Eric Nantz <thercast@gmail.com>
2021-05-25 15:08:01 -0400

    try changing the outputOptions for histogram

--- small_app/service/app.R
+++ small_app/service/app.R
@@ -4,6 +4,8 @@ server <- function(input, output) {
   output$distPlot <- renderPlot({
     hist(rnorm(input$obs), col = 'darkgray', border = 'white')
   })
+
+  outputOptions(output, "distPlot", suspendWhenHidden = FALSE)
 }
 
 ui <- fluidPage(
